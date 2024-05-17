cc19eda7e920040120911feb0f93343a578e9f74
spidy.G <120951553+JayKalt@users.noreply.github.com>
2024-05-17 19:25:19 +0200

    UPDATE MAIN
    Main file almost ultimated

--- pianificatore.c
+++ pianificatore.c
@@ -1,6 +1,29 @@
+/*
+ * NOTE:
+ * Il flag DEBUG in prossimita di alcune funzioni indica che sono funzioni
+ * utili solo durante la fase di debug e quindi sono commentate se il
+ * programma va eseguito normalmente
+ * 
+ * CASISTICHE DA RIVEDERE
+ * 1. Se le slot totali dei prodotti superano le slot massime (100)
+ * 2. Modalita di uscita dal file
+ * 3. Punto bonus - I risultati non sono salvati da nessuna parte
+ * 4. Allocazione dinamica degli array?
+ * 5. Menu di visualizzazione
+ * 
+ * DOMANDA FINALE:
+ * E' realizzabile in ASM o ci sono modifiche da fare?
+ *
+ */
+
 #include <stdio.h>
 
 void visualizza_prodotti(int, int*, int*, int*, int*);
+
+void edf(int, int*, int*, int*, int*);
+void hpf(int, int*, int*, int*, int*);
+void output(int, int*, int*, int*, int*);
+
 int main(int argc, char **argv){
 	
 	FILE *fp;
@@ -12,42 +35,54 @@ int main(int argc, char **argv){
 	
 	int n_prod = 0;
 	
+	// LETTURA FILE
 	if(argc == 2){
 		fp = fopen(argv[1], "r");
 		if(fp){
-			printf("-- FILE LETTO CORRETTAMENTE --\n\n");					  				//debug
-			while (!feof(fp)){
-				fscanf(fp, "%d", &id_prodotto[n_prod]);
-				fscanf(fp, "%d", &slot_produzione[n_prod]);
-				fscanf(fp, "%d", &scadenza[n_prod]);
-				fscanf(fp, "%d", &priorita[n_prod]);
+			
+			//printf("\n\n------ FILE LETTO CORRETTAMENTE -----\n");									//DEBUG
+
+			// SCANSIONE VALORI
+			while(!feof(fp)){
+				fscanf(fp, "%d,%d,%d,%d", &id_prodotto[n_prod], &slot_produzione[n_prod], &scadenza[n_prod], &priorita[n_prod]);
 				n_prod++;
 			}
-			
-			printf("Numero prodotti nel file: %d\n\n", n_prod);								//debug
-			visualizza_prodotti(n_prod, id_prodotto, slot_produzione, scadenza, priorita);	//debug 
+
+			//printf("Numero prodotti nel file: %d\n", n_prod);											//DEBUG
+			//visualizza_prodotti(n_prod, id_prodotto, slot_produzione, scadenza, priorita);			//DEBUG
+
 				
-			/*
+
+			// ALGORITMI
 			int algoritmo;
-			printf("1. EDF\n2. HPF\n");
+			printf("--- MENU PRINCIPALE ---\nScegli algoritmo:\n 1. EDF\n 2. HPF\n\n");
 			do{
-				printf("Scegli algoritmo: ");
+				printf("  --> ");
 				scanf("%d", &algoritmo);
-				if(algoritmo == 1)
-					printf("Algoritmo EDF...\n");
-				else if(algoritmo == 2)
-					printf("Algoritmo HPF...\n");
-				else
+				if(algoritmo == 1){
+					//ALGORITMO ERLIEST DEADLINE FRIST (EDF)
+					edf(n_prod, id_prodotto, slot_produzione, scadenza, priorita);
+					printf("\n\nPianificazione EDF:\n");
+					//visualizza_prodotti(n_prod, id_prodotto, slot_produzione, scadenza, priorita);	//DEBUG
+				}else if(algoritmo == 2){
+					//ALGORITMO HIGHER PRIORITY FIRST (HPF)
+					hpf(n_prod, id_prodotto, slot_produzione, scadenza, priorita);
+					printf("\n\nPianificazione HPF:\n");
+					//visualizza_prodotti(n_prod, id_prodotto, slot_produzione, scadenza, priorita);	//DEBUG
+				}else
 					printf("Algoritmo non valido\n");
 			}while(algoritmo > 2 || algoritmo < 1);
-			*/
+
 			
-		fclose(fp);
+			// STATS
+			output(n_prod, id_prodotto, slot_produzione, scadenza, priorita); 
+			
+			fclose(fp);
 		}else
 			printf("Errore apertura file\n");
 	}else
 		printf("Errore input argomenti\n");
-	
+			
 	
 
 	return 0;
@@ -56,53 +91,92 @@ int main(int argc, char **argv){
 void visualizza_prodotti(int n_prod, int *id_prodotto, int *slot_produzione, int *scadenza, int *priorita){
 	
 	for(int i = 0; i < n_prod; i++){
-		printf("%d,", id_prodotto[i]);
-		printf("%d,", slot_produzione[i]);
-		printf("%d,", scadenza[i]);
-		printf("%d\n", priorita[i]);
+		printf("%3d:", id_prodotto[i]);
+		printf("%2d,", slot_produzione[i]);
+		printf("%2d,", scadenza[i]);
+		printf("%2d\n", priorita[i]);
+	}
+	printf("\n");
+} 
+
+void edf(int n_prod, int *id_prodotto, int *slot_produzione, int *scadenza, int *priorita){
+	
+	int tmp;
+	
+	for(int i = 0; i < n_prod - 1; i++){
+		for(int j = 0; j < n_prod - i - 1; j++){
+			if(scadenza[j] > scadenza[j + 1] || (scadenza[j] == scadenza[j + 1] && priorita[j] < priorita[j + 1])){
+				
+				// Scambio id_prodotto
+				tmp = id_prodotto[j];
+				id_prodotto[j] = id_prodotto[j + 1];
+				id_prodotto[j + 1] = tmp;
+				
+				// Scambio slot_produzione
+				tmp = slot_produzione[j];
+				slot_produzione[j] = slot_produzione[j + 1];
+				slot_produzione[j + 1] = tmp;
+				
+				// Scambio scadenza
+				tmp = scadenza[j];
+				scadenza[j] = scadenza[j + 1];
+				scadenza[j + 1] = tmp;
+				
+				// Scambio priorita
+				tmp = priorita[j];
+				priorita[j] = priorita[j + 1];
+				priorita[j + 1] = tmp;
+			
+			}
+		}
 	}
 }
+
+void hpf(int n_prod, int *id_prodotto, int *slot_produzione, int *scadenza, int *priorita){
 	
+	int tmp;
 	
-	
-/* CHATGPT > VERSIONE SUA
-#include <stdio.h>
-
-void visualizza_prodotti(int, int*, int*, int*, int*);
-
-int main(int argc, char **argv) {
-    FILE *fp;
-    int id_prodotto[10];
-    int slot_produzione[10];
-    int scadenza[10];
-    int priorita[10];
-    int n_prod = 0;
-
-    if (argc == 2) {
-        fp = fopen(argv[1], "r");
-        if (fp) {
-            printf("-- FILE LETTO CORRETTAMENTE --\n\n");
-            while (n_prod < 10 && fscanf(fp, "%d %d %d %d", &id_prodotto[n_prod], &slot_produzione[n_prod], &scadenza[n_prod], &priorita[n_prod]) == 4) {
-                n_prod++;
-            }
-
-            printf("Numero prodotti nel file: %d\n\n", n_prod);
-            visualizza_prodotti(n_prod, id_prodotto, slot_produzione, scadenza, priorita);
-
-            fclose(fp);
-        } else {
-            printf("Errore nell'apertura del file.\n");
-        }
-    } else {
-        printf("Errore negli argomenti di input.\n");
-    }
-
-    return 0;
+	for(int i = 0; i < n_prod - 1; i++){
+		for(int j = 0; j < n_prod - i - 1; j++){
+			if(priorita[j] < priorita[j + 1] || (priorita[j] == priorita[j + 1] && scadenza[j] > scadenza[j + 1])){
+				
+				// Scambio id_prodotto
+				tmp = id_prodotto[j];
+				id_prodotto[j] = id_prodotto[j + 1];
+				id_prodotto[j + 1] = tmp;
+				
+				// Scambio slot_produzione
+				tmp = slot_produzione[j];
+				slot_produzione[j] = slot_produzione[j + 1];
+				slot_produzione[j + 1] = tmp;
+				
+				// Scambio scadenza
+				tmp = scadenza[j];
+				scadenza[j] = scadenza[j + 1];
+				scadenza[j + 1] = tmp;
+				
+				// Scambio priorita
+				tmp = priorita[j];
+				priorita[j] = priorita[j + 1];
+				priorita[j + 1] = tmp;
+			
+			}
+		}
+	}
 }
 
-void visualizza_prodotti(int n_prod, int *id_prodotto, int *slot_produzione, int *scadenza, int *priorita) {
-    for (int i = 0; i < n_prod; i++) {
-        printf("%d, %d, %d, %d\n", id_prodotto[i], slot_produzione[i], scadenza[i], priorita[i]);
-    }
+void output(int n_prod, int *id_prodotto, int *slot_produzione, int *scadenza, int *priorita){
+	
+	int penalty = 0;
+	int slot_totali = 0;
+
+	for(int i = 0; i < n_prod; i++){
+		printf("%d:%d\n", id_prodotto[i], slot_totali);
+		slot_totali += slot_produzione[i];
+		if(slot_totali > scadenza[i])
+			penalty = (slot_totali - scadenza[i]) * priorita[i];
+	}
+
+	printf("Conclusione: %d\n", slot_totali);
+	printf("Penalty: %d$\n\n", penalty);
 }
-*/ 
