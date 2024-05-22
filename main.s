bf17b12e68009bf8bbe1a4de387c3f683461d11d
JayKalt <120951553+JayKalt@users.noreply.github.com>
2024-05-22 16:55:08 +0200

    Uploaded new main file
    Ho cambiato il main file con uno nuovo per modifiche estetiche

--- main.s
+++ main.s
@@ -3,25 +3,28 @@
 # ---------------- #
 
 
-.section .data
-	filename_pointer: .int		# Filename pointer
-	fd: .int 0					# File descriptor
-	buffer: .string ""			# Spazio per buffer input
-	newline: .byte 10			# Valore simbolo nuova linea
-	pord_num:	.int 0			# Numero del prodotto
+# Dichiaraizone variabili
 
-.section .bss
+.section .data
+	filename_pointer:	.int			# Filename pointer
+	fd:					.int 0			# File descriptor
+	buffer:				.string ""		# Spazio per buffer input
+	newline:			.byte 10		# Valore simbolo nuova linea
+	pord_num:			.int 0			# Numero del prodotto
+
+
+# Codice sorgente
 
 .section .text
 	.global _start
 
 _opne:
-    # Salvo il nome del file
-	popl %ecx					# Numero di argomenti passati al programma
-	cmp $2, %ecx				# Verifica se è stato passato almeno un argomento
-	jne _exit					# Se non ci sono argomenti, esci
-	popl %ecx					# Puntatore al primo argomento (nome del programma)
-	popl filename_pointer		# Puntatore al secondo argomento (nome del file)
+	# Salvo il nome del file
+	popl %ecx						# Numero di argomenti passati al programma
+	cmp $2, %ecx					# Verifica se è stato passato almeno un argomento
+	jne _exit						# Se non ci sono argomenti, esci
+	popl %ecx						# Puntatore al primo argomento (nome del programma)
+	popl filename_pointer			# Puntatore al secondo argomento (nome del file)
 
 	# Apro il file
 	movl $5, %eax					# Syscall file open
@@ -38,44 +41,44 @@ _opne:
 
 _read_loop:
 	# Leggo il file
-	movl $3, %eax			# Syscall read
-	movl fd, %ebx			# File descriptor
-	movl $buffer, %ecx		# Buffer di input
-	movl $1, %edx			# Lunghezza massima
-	int $0x80				# Kernel interrupt
+	movl $3, %eax					# Syscall read
+	movl fd, %ebx					# File descriptor
+	movl $buffer, %ecx				# Buffer di input
+	movl $1, %edx					# Lunghezza massima
+	int $0x80						# Kernel interrupt
 
-	cmp $0, %eax			# Verifico no errori / EOF
-	jle _close_file			# E in caso chiudo il file
+	cmp $0, %eax					# Verifico no errori / EOF
+	jle _close_file					# E in caso chiudo il file
 
 	# Controllo se ho una nuova linea
-	movb buffer, %al		# Copio il carattere nel buffer in AL
-	cmpb newline, %al		# Comparo il carattere con '\n'
-	jne _print_line			# Se sono diversi stampo il carattere
-	incw pord_num			# Altrimenti incremento il counter
+	movb buffer, %al				# Copio il carattere nel buffer in AL
+	cmpb newline, %al				# Comparo il carattere con '\n'
+	jne _print_line					# Se sono diversi stampo il carattere
+	incw pord_num					# Altrimenti incremento il counter
 
 _print_line:
 	# Stampa il contenuto della riga
-	movl $4, %eax			# Syscall write
-	movl $1, %ebx			# File descriptor stdout (terminale)
-	movl $buffer, %ecx		# Buffer di output
-	int $0x80				# Kernel interrupt
+	movl $4, %eax					# Syscall write
+	movl $1, %ebx					# File descriptor stdout (terminale)
+	movl $buffer, %ecx				# Buffer di output
+	int $0x80						# Kernel interrupt
 
-	jmp _read_loop			# Ricomincia la lettura file
+	jmp _read_loop					# Ricomincia la lettura file
 
 
 
 _close_file: 
 	# Chiudo il file
-	movl $6, %eax			# Syscall close
-	movl %ebx, %ecx			# File descriptor da chiudere
-	int $0x80				# Kernel interrupt 
+	movl $6, %eax					# Syscall close
+	movl %ebx, %ecx					# File descriptor da chiudere
+	int $0x80						# Kernel interrupt 
 
 _exit:
 	# Uscita programma
-	movl $1, %eax			# Syscall exit
-	xorl %ebx, %ebx			# Codice di usita 0
-	int $0x80				# Kernel interrupt
+	movl $1, %eax					# Syscall exit
+	xorl %ebx, %ebx					# Codice di usita 0
+	int $0x80						# Kernel interrupt
 
 _start:
-	jmp _opne				# Passa ad open
-	jmp _exit				# Passa ad exit
+	jmp _opne						# Passa ad open
+	jmp _exit						# Passa ad exit
